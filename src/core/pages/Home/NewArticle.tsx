a5ec833313c85eddc554214c2e6545ff21910ce2
Ha_LN <ha_ln@EVN084-HaLN.local>
2021-06-22 17:35:35 +0700

    fixed a post

--- src/core/pages/Home/NewArticle.tsx
+++ src/core/pages/Home/NewArticle.tsx
@@ -2,7 +2,66 @@ function NewArticle(){
     return <div className="new-article">
             <h2>Bài viết mới</h2>
             <ul>
-                
+                <li>
+                    <img 
+                        src="https://cdn.dribbble.com/users/808903/screenshots/3831862/dribbble_szablon__1_1.png?compress=1&resize=400x300" 
+                        alt="Nestjs" />
+                    <div className="content">
+                        <div className="tag">
+                            <span>Nestjs</span>
+                            <span>Backend</span>
+                            <span>Nodejs</span>
+                        </div>
+                        <h3 className="title"> NEW FRAME FOR NODEJS </h3>
+                        <div className="auth-time">
+                            <span>
+                                <i className="fa fa-user" aria-hidden="true"></i>
+                                Le Ngoc Ha
+                            </span>
+                            <span>
+                                <i className="fa fa-clock-o" aria-hidden="true"></i>
+                                2020-12-02
+                            </span>
+                            <span>
+                                <i className="fa fa-comments" aria-hidden="true"></i>
+                                5
+                            </span>
+                        </div>
+                        <p className="short-content">Hàm này actionService.checkExitsElement(actionId, listAction) được gọi đến nhiều lần,
+                         em nên gán cho một biến nhé.Hàm này actionService.checkExitsElement(actionId, listAction) được gọi đến nhiều lần, em nên gán cho một biến nhé.</p>
+                        <button>Read more</button>
+                    </div>
+                </li>
+                <li>
+                    <img 
+                        src="https://cdn.dribbble.com/users/808903/screenshots/3831862/dribbble_szablon__1_1.png?compress=1&resize=400x300" 
+                        alt="Nestjs" />
+                    <div className="content">
+                        <div className="tag">
+                            <span>Nestjs</span>
+                            <span>Backend</span>
+                            <span>Nodejs</span>
+                        </div>
+                        <h3 className="title"> NEW FRAME FOR NODEJS </h3>
+                        <div className="auth-time">
+                        <span>
+                                <i className="fa fa-user" aria-hidden="true"></i>
+                                Le Ngoc Ha
+                            </span>
+                            <span>
+                                <i className="fa fa-clock-o" aria-hidden="true"></i>
+                                2020-12-02
+                            </span>
+                            <span>
+                                <i className="fa fa-comments" aria-hidden="true"></i>
+                                5
+                            </span>
+                        </div>
+                        <p className="short-content">Hàm này actionService.checkExitsElement(actionId, listAction) được gọi đến nhiều lần,
+                         em nên gán cho một biến nhé.Hàm này actionService.checkExitsElement(actionId, listAction) được gọi đến nhiều lần, em nên gán cho một biến nhé.</p>
+                        <button>Read more</button>
+                    </div>
+                </li>
             </ul>
     </div>
 }
--- src/core/pages/Home/index.tsx
+++ src/core/pages/Home/index.tsx
@@ -1,20 +1,20 @@
 import { useEffect } from 'react';
 import Layout from '../Layout';
 import Banner from '../../components/common/Banner/Banner';
-// import CropDemo from '../../components/common/cropperImage'
-// import ModalBase from '../../components/common/modal'
-// import {useDispatch} from 'react-redux'
-// import {LOAD_TODO_FAKE} from '../../store/sagaActions'
+import {useDispatch} from 'react-redux'
+import {LOAD_TODO_FAKE} from '../../store/sagaActions'
 import NewArticle from './NewArticle'
 import PerfectArticl from './PerfectArticle'
 import './home.scss'
+import { useCallback } from 'react';
 
 function Home() {
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
+  const fetchUser = useCallback(() => dispatch({type: LOAD_TODO_FAKE}), [dispatch])
   useEffect(() => {
     document.title = 'Blog programming';
-  }, []);
-  // const fetchUser = () => dispatch({type: LOAD_TODO_FAKE})
+    fetchUser()
+  }, [fetchUser]);
   return (
     <Layout>
       <>
@@ -22,10 +22,6 @@ function Home() {
        <div className="body-home">
          <NewArticle />
          <PerfectArticl />
-            {/* <ModalBase>
-              <CropDemo src="https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-4.jpg" />
-            </ModalBase>
-            <button onClick={fetchUser}>Load user</button> */}
        </div>
       </>
     </Layout>
