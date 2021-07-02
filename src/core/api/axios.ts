91580f01164a260bd0e037c1cf24e195b0e105fe
Ha_LN <ha_ln@EVN084-HaLN.local>
2021-07-02 16:22:41 +0700

    connect backend by update axios change timeouts

--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -3,6 +3,6 @@ export const instanceAxios = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? 
     'https://blog-programming.herokuapp.com/api/' : 
     'http://localhost:9000/api/',
-    timeout: 5000,
+    timeout: 20000,
     withCredentials: true
 });
